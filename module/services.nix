85cd527edf44314b9a7dfe811098bdf27abf41bc
Litchi Pi <litchi.pi@proton.me>
2023-06-01 10:23:01 +0200

    fixup
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- module/services.nix
+++ module/services.nix
@@ -31,7 +31,7 @@ in {
     serviceConfig.Type = "oneshot";
     script = let
       # Go through the secrets config and set permissions based on their configuration set
-      set_perms_and_link = _: data: if (builtins.hasAttr "__is_leaf" data && data.enable)
+      set_perms_and_link = _: data: if ((builtins.hasAttr "__is_leaf" data) && data.enable)
         then (''
           if [ -f ${data.file} ]; then
             chmod 400 ${data.file}
