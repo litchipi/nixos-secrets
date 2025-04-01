ed03418a5b3ef9cf96b3ce21534b167617412439
Litchi Pi <litchi.pi@proton.me>
2025-04-01 14:37:03 +0200

    Remove unused secret_file option
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- lib/types.nix
+++ lib/types.nix
@@ -1,30 +1,38 @@
 { lib, ... }: {
   secretType = name: lib.types.submodule {
     options = {
+      __is_leaf = lib.mkOption {
+        visible = false;
+        type = lib.types.bool;
+      };
+
+      name = lib.mkOption {
+        visible = false;
+        type = lib.types.str;
+      };
+
       enable = lib.mkEnableOption {
-        description = ''
-          If set to true, will decrypt the secret file, if not it will stay encrypted and be unusable.
-        '';
+        visible = false;
       };
+
       file = lib.mkOption {
+        visible = false;
         type = lib.types.path;
-        description = "Where to decrypt the secret ${name}";
       };
+
       age_enc_data = lib.mkOption {
+        visible = false;
         type = lib.types.str;
-        description = "Encrypted data for the secret ${name}";
       };
+
       link = lib.mkOption {
+        visible = false;
         type = lib.types.nullOr lib.types.path;
-        description = "Destination where to create a symlink for the secret file ${name}";
       };
+
       transform = lib.mkOption {
+        visible = false;
         type = lib.types.str;
-        description = ''
-          Bash code to transform ${name} before saving to file.
-          The secret will be piped into the code:
-            <decrypt> | <transformation code> > /path/to/destination
-        '';
       };
     };
   };
--- module/services.nix
+++ module/services.nix
@@ -1,31 +1,9 @@
 seclib: {config, lib, pkgs, ...}: let
   cfg = config.secrets;
-
-  target_bind = "local-fs.target";
 in {
-  create-secret-files = {
-    script = builtins.concatStringsSep "\n\n" (lib.attrsets.mapAttrsToList
-      (name: f: let
-          group = if builtins.isNull f.group then f.user else f.group;
-        in (if !f.enable then "" else ''
-          mkdir -p $(dirname ${seclib.getSecretFileDest name})
-          cat << EOF > ${seclib.getSecretFileDest name}
-          ${f.text}
-          EOF
-          chown ${f.user}:${group} ${seclib.getSecretFileDest name}
-          chmod ${f.perms} ${seclib.getSecretFileDest name}
-        ''))
-        cfg.secret_files);
-    after = [target_bind];
-    wantedBy = [target_bind];
-    # requiredBy = [target_bind];
-    serviceConfig.Type = "oneshot";
-  };
-
   # Service triggered when setting the system, will set up the right permissions
   #   for all the secrets
   setup-secrets-perms-and-links = {
-    # requiredBy = [ target_bind ];
     wantedBy = [ "local-fs.target" ];
     after = [ "local-fs.target" ];
     serviceConfig.Type = "oneshot";
